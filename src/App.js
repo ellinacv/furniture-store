cb3e61e683dedfaa9ae95e8fbb6716b374c2d2b2
ellinacv <113457842+ellinacv@users.noreply.github.com>
2023-10-06 19:26:56 +0300

    feat: create new block (Products)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Header } from './components/Header/Header';
 import { Intro } from './components/Intro/Intro';
+import { Features } from './components/Features/Features';
 
 import './styles/main.scss';
+import { Products } from './components/Products/Products';
 
 export const App = () => {
   return (
     <>
       <Header />
       <Intro />
+      <Features />
+      <Products />
     </>
   );
 };
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import s from './Products.module.scss';
+
+import card1 from '../../images/products/card_01.jpg';
+import card2 from '../../images/products/card_02.jpg';
+import card3 from '../../images/products/card_03.jpg';
+import card4 from '../../images/products/card_04.jpg';
+import card5 from '../../images/products/card_05.jpg';
+
+import { Like } from '../../images/svg/Like';
+
+const cards = [
+  {
+    title: 'The Dandy chair',
+    price: 250,
+    image: card1,
+  },
+  {
+    title: 'Rustic Vase Set',
+    price: 155,
+    image: card2,
+  },
+  {
+    title: 'The Silky Vase',
+    price: 125,
+    image: card3,
+  },
+  {
+    title: 'The Lucy Lamp',
+    price: 300,
+    image: card4,
+  },
+  {
+    title: 'The Dandy chair',
+    price: 250,
+    image: card5,
+  },
+  {
+    title: 'The Dandy chair',
+    price: 250,
+    image: card4,
+  },
+  {
+    title: 'Rustic Vase Set',
+    price: 155,
+    image: card2,
+  },
+  {
+    title: 'The Silky Vase',
+    price: 125,
+    image: card3,
+  },
+];
+
+export const Products = () => {
+  return (
+    <section className={s.wrap}>
+      <div className="container">
+        <h2 className={`${s.title} title`}>Our products</h2>
+        <div className={s.cards}>
+          {cards.map((card) => (
+            <div className={s.cards__item}>
+              <div className={s.cards__item__img}>
+                <img src={card.image} alt={card.title} />
+                <Like className={s.like} color="white" />
+              </div>
+              <h5>{card.title}</h5>
+              <span>{card.price} $</span>
+            </div>
+          ))}
+        </div>
+        <div className={s.btn}>
+          <a href="/#" className="btn">
+            View collection
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
